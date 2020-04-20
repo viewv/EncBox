2be7d77cc093e3301c4838c38b2e46436d5d952f
viewv <zxnnet@gmail.com>
2020-04-20 10:59:58 +0800

    Test Generate

--- src/main/java/top/viewv/model/tools/GenerateSecKey.java
+++ src/main/java/top/viewv/model/tools/GenerateSecKey.java
@@ -1,5 +1,43 @@
 package top.viewv.model.tools;
 
-public class addSaltToPassword {
-    
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class GenerateSecKey{
+    public static SecretKey generateKey(String password ,int keylength,
+                                        int interationcount,int mode,String algorithm){
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256","BC");
+
+            byte[] salt;
+
+            if (mode == 0){
+                // generate salt
+                salt = SecureRandom.getInstanceStrong().generateSeed(16);
+            }else {
+                // not really safe to use a fixed salt!
+                salt = "1234567890encbox".getBytes(StandardCharsets.UTF_8);
+            }
+            System.out.printf("salt: %032x\n", new BigInteger(1, salt));
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, interationcount, keylength);
+            SecretKey tmp = factory.generateSecret(spec);
+            return new SecretKeySpec(tmp.getEncoded(), algorithm);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/test/java/toolstest/GenerateKeyTest.java
+++ src/test/java/toolstest/GenerateKeyTest.java
@@ -1,4 +1,23 @@
 package toolstest;
 
+import top.viewv.model.tools.GenerateSecKey;
+import top.viewv.model.tools.PasswordGenerate;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 public class GenerateKeyTest {
+    public static void main(String[] args) {
+        String password = PasswordGenerate.generatePassword(
+                2,4,
+                2,4,15);
+        System.out.println(password);
+        System.out.println(password.length());
+
+        SecretKey secretKey = GenerateSecKey.generateKey(password,256,65566,
+                                                        0,"ChaCha20-Poly1305");
+        assert secretKey != null : "key is empty!";
+        String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        System.out.println(encodedKey);
+    }
 }
