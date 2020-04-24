ef9d8b59679156ad1056e7c826bb799cb1e0528b
viewv <zxnnet@gmail.com>
2020-04-24 23:30:20 +0800

    Offically migrate to java14 test keyfile generate

--- src/main/java/top/viewv/model/tools/GenerateSymmetricKeyFile.java
+++ src/main/java/top/viewv/model/tools/GenerateSymmetricKeyFile.java
@@ -1,4 +1,51 @@
 package top.viewv.model.tools;
 
+import javax.crypto.SecretKey;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class GenerateSymmetricKeyFile {
+    public static void generate(String destFilepath, String algorithm,int keylength,
+                                SecretKey key, byte[] iv) throws FileNotFoundException {
+
+        RandomAccessFile file = new RandomAccessFile(destFilepath, "rw");
+        byte head = 0b0;
+        byte[] keybyte = key.getEncoded();
+
+        //TODO 3bit about algorithm
+        switch (algorithm) {
+            case "AES-CBC":
+                head += 0b00100000;
+                break;
+            case "AEC-CFB":
+                head += 0b01000000;
+                break;
+            case "AES-GCM":
+                head += 0b01100000;
+                break;
+        }
+
+        switch (keylength) {
+            case 128:
+                head += 0b00001000;
+                break;
+            case 256:
+                head += 0b00010000;
+                break;
+            case 512:
+                head += 0b00011000;
+                break;
+        }
+        //TODO Add True thing!
+
+        try {
+            file.writeByte(head);
+            file.write(keybyte);
+            file.write(iv);
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/toolstest/GenerateKeyTest.java
+++ src/test/java/toolstest/GenerateKeyTest.java
@@ -1,22 +1,31 @@
 package toolstest;
 
 import top.viewv.model.tools.GenerateSecKey;
+import top.viewv.model.tools.GenerateSymmetricKeyFile;
 import top.viewv.model.tools.PasswordGenerate;
 
 import javax.crypto.SecretKey;
+import java.io.FileNotFoundException;
 import java.util.Base64;
 
 public class GenerateKeyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        String currentPath = System.getProperty("user.dir");
+        
         String password = PasswordGenerate.generatePassword(
                 2,5,
                 4,5,20);
         System.out.println(password);
         System.out.println(password.length());
+        
+        byte[] iv = new byte[0];
 
         SecretKey secretKey = GenerateSecKey.generateKey(password,256,65566,
                                                         1,"ChaCha20-Poly1305");
-        assert secretKey != null : "key is empty!";
+        assert secretKey != null;
+        GenerateSymmetricKeyFile.generate(currentPath + "/src/test/java/toolstest/test.key",
+                "AES-CBC",256,secretKey,iv);
         String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         System.out.println(encodedKey);
     }
