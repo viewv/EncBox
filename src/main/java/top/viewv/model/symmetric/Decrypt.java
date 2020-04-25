0c66cae7c12f97a308f2855f67b5d68e74c51f10
viewv <zxnnet@gmail.com>
2020-04-25 21:58:47 +0800

    Fix bug

--- src/main/java/top/viewv/model/symmetric/Decrypt.java
+++ src/main/java/top/viewv/model/symmetric/Decrypt.java
@@ -25,61 +25,71 @@ public class Decrypt {
             byte head = heads[0];
 
             int algoMask = head & 0b11110000;
-            int keyMask =  head & 0b00001100;
-            int ivMask =   head & 0b00000011;
+            int keyMask = head & 0b00001100;
+            int ivMask = head & 0b00000011;
 
-            int ivLength,keyLength;
+            int ivLength, keyLength;
 
-            String algorithm = "";
+            String algorithm;
 
-            switch (algoMask){
+            switch (algoMask) {
                 case 0b00010000 -> algorithm = "AES/CBC/PKCS7Padding";
                 case 0b00100000 -> algorithm = "AES/CFB/NoPadding";
                 case 0b00110000 -> algorithm = "AES/CTR/NoPadding";
                 case 0b01000000 -> algorithm = "AES/CBC/CS3Padding";
                 case 0b01010000 -> algorithm = "AES/GCM/NoPadding";
                 case 0b01100000 -> algorithm = "AES/CCM/NoPadding";
+                default -> {
+                    body.close();
+                    throw new IllegalStateException("Unexpected AlgoMask: " + algoMask);
+                }
             }
 
             System.out.println(algorithm);
 
-            switch (keyMask){
+            switch (keyMask) {
                 case 0b00000100 -> keyLength = 16;
                 case 0b00001000 -> keyLength = 32;
                 case 0b00001100 -> keyLength = 64;
-                default -> throw new IllegalStateException("Unexpected value: " + keyMask);
+                default -> {
+                    body.close();
+                    throw new IllegalStateException("Unexpected value: " + keyMask);
+                }
             }
 
-            System.out.println("Key Length: "+ keyLength);
+            System.out.println("Key Length: " + keyLength);
 
-            switch (ivMask){
+            switch (ivMask) {
                 case 0b00000001 -> ivLength = 16;
                 case 0b00000010 -> ivLength = 32;
                 case 0b00000011 -> ivLength = 64;
-                default -> throw new IllegalStateException("Unexpected value: " + ivMask);
+                default -> {
+                    body.close();
+                    throw new IllegalStateException("Unexpected value: " + ivMask);
+                }
             }
 
-            System.out.println("IV Length: "+ivLength);
+            System.out.println("IV Length: " + ivLength);
 
             byte[] iv = new byte[ivLength];
 
-            body.readNBytes(iv,0,ivLength);
+            body.readNBytes(iv, 0, ivLength);
 
-            Cipher cipher = Cipher.getInstance(algorithm,"BC");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey,new IvParameterSpec(iv));
+            Cipher cipher = Cipher.getInstance(algorithm, "BC");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
 
-            CipherInputStream is = new CipherInputStream(body,cipher);
+            CipherInputStream is = new CipherInputStream(body, cipher);
 
             byte[] filenameLengthBytes = is.readNBytes(1);
             int filenameLength = filenameLengthBytes[0];
 
-            System.out.println("Decrypt Filename Length: "+filenameLength);
+            System.out.println("Decrypt Filename Length: " + filenameLength);
 
             byte[] filenameBytes = is.readNBytes(filenameLength);
 
             String filename = new String(filenameBytes, StandardCharsets.UTF_8);
 
-            System.out.println(filename+": Length: "+filename.length());
+            System.out.println(filename + ": Length: " + filename.length());
 
             OutputStream out = new FileOutputStream(destfilepath + filename);
 
@@ -87,8 +97,10 @@ public class Decrypt {
 
             is.close();
             out.close();
+            body.close();
 
-        } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException
+                | InvalidAlgorithmParameterException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/top/viewv/model/symmetric/Encrypt.java
+++ src/main/java/top/viewv/model/symmetric/Encrypt.java
@@ -14,12 +14,13 @@ import java.security.spec.InvalidParameterSpecException;
 
 public class Encrypt {
 
-    public static byte[] encrypt(String sourcefilepath,String sourcefilename, String destfile,String algorithm, SecretKey secretKey) {
+    public static byte[] encrypt(String sourcefilepath, String sourcefilename, String destfile, String algorithm,
+            SecretKey secretKey) {
 
         Security.addProvider(new BouncyCastleProvider());
 
         try {
-            Cipher cipher = Cipher.getInstance(algorithm,"BC");
+            Cipher cipher = Cipher.getInstance(algorithm, "BC");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             AlgorithmParameters params = cipher.getParameters();
 
@@ -33,11 +34,16 @@ public class Encrypt {
 
             switch (algorithm) {
                 case "AES/CBC/PKCS7Padding" -> head += 0b00010000;
-                case "AES/CFB/NoPadding"    -> head += 0b00100000;
-                case "AES/CTR/NoPadding"    -> head += 0b00110000;
-                case "AES/CBC/CS3Padding"   -> head += 0b01000000;
-                case "AES/GCM/NoPadding"    -> head += 0b01010000;
-                case "AES/CCM/NoPadding"    -> head += 0b01100000;
+                case "AES/CFB/NoPadding" -> head += 0b00100000;
+                case "AES/CTR/NoPadding" -> head += 0b00110000;
+                case "AES/CBC/CS3Padding" -> head += 0b01000000;
+                case "AES/GCM/NoPadding" -> head += 0b01010000;
+                case "AES/CCM/NoPadding" -> head += 0b01100000;
+                default -> {
+                    body.close();
+                    is.close();
+                    throw new IllegalStateException("Unexpected algorithm: " + algorithm);
+                }
             }
 
             byte[] keybytes = secretKey.getEncoded();
@@ -47,18 +53,28 @@ public class Encrypt {
                 case 16 -> head += 0b00000100;
                 case 32 -> head += 0b00001000;
                 case 64 -> head += 0b00001100;
+                default -> {
+                    body.close();
+                    is.close();
+                    throw new IllegalStateException("Unexpected keylength: " + keylength);
+                }
             }
 
-            switch (iv.length){
+            switch (iv.length) {
                 case 16 -> head += 0b00000001;
                 case 32 -> head += 0b00000010;
                 case 64 -> head += 0b00000011;
+                default -> {
+                    body.close();
+                    is.close();
+                    throw new IllegalStateException("Unexpected IVlength: " + iv.length);
+                }
             }
 
             body.write(head);
             body.write(iv);
 
-            CipherOutputStream out = new CipherOutputStream(body,cipher);
+            CipherOutputStream out = new CipherOutputStream(body, cipher);
 
             byte[] filename = sourcefilename.getBytes();
             int filenameLength = filename.length;
@@ -72,10 +88,11 @@ public class Encrypt {
 
             is.close();
             out.close();
-
+            body.close();
             return iv;
 
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | NoSuchProviderException | InvalidKeyException | InvalidParameterSpecException e) {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | NoSuchProviderException | InvalidKeyException
+                | InvalidParameterSpecException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
