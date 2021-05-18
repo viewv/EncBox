460981f39e410e2124ee3fe69477f3442bb39973
viewv <zxnnet@gmail.com>
2021-05-18 21:52:45 +0800

    Finish password

--- src/main/java/top/viewv/controller/ItemController.java
+++ src/main/java/top/viewv/controller/ItemController.java
@@ -1,8 +1,11 @@
 package top.viewv.controller;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+import java.util.Optional;
 
 public class ItemController {
 
@@ -27,6 +30,56 @@ public class ItemController {
     }
 
     public void modify(MouseEvent mouseEvent) {
+        String password;
 
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setTitle("Modify");
+        dialog.setHeaderText("Modify Password");
+
+        ButtonType buttonType = new ButtonType("Modify", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(buttonType, ButtonType.CANCEL);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20, 20, 10, 10));
+
+        PasswordField pwd = new PasswordField();
+        pwd.setPromptText("password");
+
+        gridPane.add(new Label("Password:"), 0, 0);
+        gridPane.add(pwd, 1, 0);
+
+        dialog.getDialogPane().setContent(gridPane);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == buttonType) {
+                return pwd.getText();
+            }
+            return null;
+        });
+
+        Optional<String> result = dialog.showAndWait();
+
+        if (result.isPresent()){
+            password = result.get();
+
+            if (password.length() != 0){
+                this.password.setText(password);
+                mainController.modifyPassword(filename.getText(),password);
+            }else {
+                sendAlert(Alert.AlertType.WARNING,
+                        "Warning",
+                        "Empty Password");
+            }
+        }
+    }
+
+    private void sendAlert(Alert.AlertType type, String title, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
